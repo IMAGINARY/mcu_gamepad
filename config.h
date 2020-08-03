2a0ec244d9c552a1342989c3558e7aab8dfe69a9
Christian Stussak <stussak@mfo.de>
2020-08-03 13:56:55 +0200

    Move settings into config.h

---
+++ config.h
@@ -0,0 +1,21 @@
+#define PIN_L 0
+#define PIN_R 1
+#define PIN_U 3
+#define PIN_D 2
+
+#define PIN_A 4
+#define PIN_B 5
+#define PIN_X 6
+#define PIN_Y 7
+
+#define BUTTON_ID_L 15
+#define BUTTON_ID_R 16
+#define BUTTON_ID_U 13
+#define BUTTON_ID_D 14
+
+#define BUTTON_ID_A 1
+#define BUTTON_ID_B 2
+#define BUTTON_ID_X 3
+#define BUTTON_ID_Y 4
+
+#define DEBOUNCE_INTERVAL 20 // milli seconds
